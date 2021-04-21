2140039ba939621d45ab00185ae68e1fdd317d90
dumi994 <dumi.9944@gmail.com>
2021-04-21 16:03:57 +0200

    cambiato titoli main content

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,24 @@
 
 //Use alert
 alert('Stiamo per cambiare titoli con JS');
+
+
 //Get elementById
-document.getElementById('heavy_metal').innerHtml ="Metallo pesante";
+document.getElementById('heavy_metal').innerHTML ="Metallo pesante";
+document.getElementById('heavy_metal2').innerHTML ="Metallo pesante";
+document.getElementById('heavy_metal3').innerHTML ="Metallo pesante";
+
+
+document.getElementById('stranger_things').innerHTML ="Cose strane";
+document.getElementById('stranger_things2').innerHTML ="Cose strane";
+document.getElementById('stranger_things3').innerHTML ="Cose strane";
+
+
+
+document.getElementById('quiet_place').innerHTML ="Un posto tranquillo";
+
+document.getElementById('split').innerHTML ="Diviso";
+
+document.getElementById('cure').innerHTML ="Il rimedio";
+
+document.getElementById('sinister').innerHTML ="Sinistro";
